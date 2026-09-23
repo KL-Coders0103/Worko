74c0c19cf7afa98322744a316d235bc1f734cd91
KL-Coders0103 <lovesh.m.bodhani@gmail.com>
2026-09-23 11:21:38 +0530

    .

--- apps/api/prisma.config.ts
+++ apps/api/prisma.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
   },
   engine: "classic",
   datasource: {
-    url: env("DATABASE_URL"),
-    shadowDatabaseUrl: env("SHADOW_DATABASE_URL"),
+    url: env("DATABASE_URL")
   },
 });
