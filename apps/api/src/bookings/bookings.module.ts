dc30a1b88b4104d5f1a23e5709bd73a9132a895f
Lovesh Mahendrakumar Bodhani <lovesh.m.bodhani@gmail.com>
2026-09-25 23:06:11 +0530

    feat(api): enable wallet release in bookings

--- apps/api/src/bookings/bookings.module.ts
+++ apps/api/src/bookings/bookings.module.ts
@@ -6,12 +6,14 @@ import {BookingsService} from './bookings.service';
 import {PrismaModule} from '../common/prisma/prisma.module';
 import {AuthModule} from '../auth/auth.module';
 import {RealtimeModule} from '../realtime/realtime.module';
+import {WalletModule} from '../wallet/wallet.module';
 
 @Module({
   imports: [
     PrismaModule,
     AuthModule,
     RealtimeModule,
+    WalletModule,
   ],
   controllers: [
     BookingsController,
