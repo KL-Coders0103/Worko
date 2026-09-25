9c09a9c18e82a0e158e360444491ff6843e5f42a
Lovesh Mahendrakumar Bodhani <lovesh.m.bodhani@gmail.com>
2026-09-25 20:40:08 +0530

    feat(api): connect requirements to realtime notifications

--- apps/api/src/requirements/requirements.module.ts
+++ apps/api/src/requirements/requirements.module.ts
@@ -2,6 +2,7 @@ import {Module} from '@nestjs/common';
 
 import {AuthModule} from '../auth/auth.module';
 import {PrismaModule} from '../common/prisma/prisma.module';
+import {RealtimeModule} from '../realtime/realtime.module';
 
 import {RequirementsController} from './requirements.controller';
 import {RequirementsService} from './requirements.service';
@@ -10,6 +11,7 @@ import {RequirementsService} from './requirements.service';
   imports: [
     PrismaModule,
     AuthModule,
+    RealtimeModule,
   ],
   controllers: [
     RequirementsController,
