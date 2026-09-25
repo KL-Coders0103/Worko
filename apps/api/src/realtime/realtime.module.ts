3afca54380bd6c982791038d01cb050938ffa03d
Lovesh Mahendrakumar Bodhani <lovesh.m.bodhani@gmail.com>
2026-09-25 20:40:05 +0530

    fix(api): provide prisma to realtime gateway

--- apps/api/src/realtime/realtime.module.ts
+++ apps/api/src/realtime/realtime.module.ts
@@ -1,12 +1,14 @@
 import {Module} from '@nestjs/common';
 
 import {AuthModule} from '../auth/auth.module';
+import {PrismaModule} from '../common/prisma/prisma.module';
 
 import {RealtimeGateway} from './realtime.gateway';
 
 @Module({
   imports: [
     AuthModule,
+    PrismaModule,
   ],
   providers: [
     RealtimeGateway,
