700c7e813b18f1b4e8519e97edd1504ff183a139
Lovesh Mahendrakumar Bodhani <lovesh.m.bodhani@gmail.com>
2026-09-26 20:49:42 +0530

    feat(mobile): improve startup and toast foundation

--- apps/mobile/index.js
+++ apps/mobile/index.js
@@ -1,1 +1,5 @@
-import {AppRegistry} from 'react-native';import App from './App';import {name as appName} from './app.json';AppRegistry.registerComponent(appName,()=>App);
+import {AppRegistry} from 'react-native';
+import App from './src/app/App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
